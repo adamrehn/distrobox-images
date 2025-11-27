4e2c4560f938e9a4bb0348fb8463176013807c2c
Adam Rehn <adam@adamrehn.com>
2025-11-27 17:23:18 +1000

    Add functionality to push images to Docker Hub

--- build.sh
+++ build.sh
@@ -3,13 +3,21 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -ex
 
 
+# Determine whether we are generating manifests that pull the images from Docker Hub
+MANIFEST_OPTIONS=''
+if [[ $* == *--live-manifests* ]]; then
+	MANIFEST_OPTIONS='--options pull=true'
+fi
+
+
 function generateManifest() {
 	local name="$1"
 	local image="$2"
 	docker run --rm "-v$SCRIPT_DIR:/hostdir" -w /hostdir -u `id -u`:`id -g` "$image" generate-manifest.py \
 		--name "$name" \
 		--image "$image" \
-		--outfile "manifests/$name.ini"
+		--outfile "manifests/$name.ini" \
+		$MANIFEST_OPTIONS
 }
 
 
@@ -22,3 +30,8 @@ docker build --progress=plain -t 'adamrehn/distrobox-swiss-army-knife:latest' "$
 # Generate the manifests for our Distrobox images
 rm -f "$SCRIPT_DIR/manifests/"*.ini
 generateManifest 'swiss-army-knife' 'adamrehn/distrobox-swiss-army-knife:latest'
+
+# Determine whether we are pushing the images to Docker Hub
+if [[ $* == *--push-images* ]]; then
+	docker push 'adamrehn/distrobox-swiss-army-knife:latest'
+fi
--- common/generate-manifest.py
+++ common/generate-manifest.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--name', required=True, help='The name for the Distrobox container')
 parser.add_argument('--image', required=True, help='The container image tag')
 parser.add_argument('--outfile', required=True, help='The path to the output manifest file')
+parser.add_argument('--options', action='append', help='Additional options to include in the manifest')
 args = parser.parse_args()
 
 # Retrieve the list of applications and binaries we should export
@@ -19,12 +20,14 @@ if 'applications' not in exports or 'binaries' not in exports or not isinstance(
 	raise RuntimeError('the container exports JSON file is malformed')
 
 # Generate our manifest
+extra_options = [] if args.options is None else args.options
 manifest = '\n'.join(
 	[
 		f'[{ args.name }]',
 		f'image="{ args.image }"',
 		'replace=true',
 	] +
+	extra_options +
 	[f'exported_apps="{ app }"' for app in exports['applications']] +
 	[f'exported_bins="{ bin }"' for bin in exports['binaries'].values()] +
 	['']
