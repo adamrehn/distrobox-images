d39a8de72a8132d59bb9a8c7f99cfc2f0751e811
Adam Rehn <adam@adamrehn.com>
2026-06-06 21:17:25 +1000

    Reset Wine scaling to 96 DPI when host is using default scale factor

--- images/bases/ubuntu2604-wine/wine-wrapper.py
+++ images/bases/ubuntu2604-wine/wine-wrapper.py
@@ -10,13 +10,19 @@ def capture(command, **kwargs):
 	return run(command, **{'capture_output': True, 'encoding': 'utf-8', **kwargs})
 
 
+# Wine's default scaling factor is 96 dpi, so we reset it to that if we don't detect a non-default scale factor from the host system
+dpi = 96
+
 # Attempt to determine the DPI scale factor used by the host system, and propagate it to Wine
 lines = capture(['xrdb', '-query']).stdout.splitlines()
 parsed = [[field.strip() for field in line.split(':')] for line in lines]
 for fields in parsed:
 	if len(fields) > 1 and fields[0] == 'Xft.dpi':
-		run(['wine', 'reg', 'add', 'HKEY_CURRENT_USER\\Control Panel\\Desktop', '/v', 'LogPixels', '/t', 'REG_DWORD', '/d', fields[1], '/f'])
+		dpi = int(fields[1])
 		break
+
+# Update Wine's scaling factor
+run(['wine', 'reg', 'add', 'HKEY_CURRENT_USER\\Control Panel\\Desktop', '/v', 'LogPixels', '/t', 'REG_DWORD', '/d', dpi, '/f'])
 
 # Pass the command-line arguments through to Wine
 sys.exit(
